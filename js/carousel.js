81f15f1733697ef6aa8f198b74c7eb7f8b444540
claudem194 <eduardoclaudiomartin@gmail.com>
2024-10-02 12:01:36 -0300

    Agregado de funcionalidad para agragar textos dentro del carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,14 @@
 let carousel = document.querySelector('.contenedor-carousel')
 
+let tituloCarousel = document.querySelector('.titulo-carousel')
+
+let parrafoCarousel = document.querySelector('.parrafo-carousel')
+
+
+let carouselTexto = document.querySelector('.carousel-textos')
+
+
+//
 
 console.log(carousel)
 
@@ -15,6 +24,16 @@ carousel.style.backgroundPosition = 'center center'
 
 
 /**********set interval como manera de automatizar******************** */
+
+let listadoTextosItems = [
+    { titulo: 'Naturaleza', parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam maxime consectetur sed obcaecati numquam ex nemo maiores nobis eveniet?' },
+    { titulo: 'Aire libre', parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam maxime consectetur sed obcaecati numquam ex nemo maiores nobis eveniet?' },
+    { titulo: 'Montañas', parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam maxime consectetur sed obcaecati numquam ex nemo maiores nobis eveniet?' },
+    { titulo: 'Excursiones', parrafo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quibusdam maxime consectetur sed obcaecati numquam ex nemo maiores nobis eveniet?' },
+
+]
+
+
 
 
 let listadoCarousel = ['1.jpg', '2.jpg', '3.jpg', '4.jpg']
@@ -32,8 +51,21 @@ setInterval(() => {
 
     carousel.style.backgroundPosition = 'center center'
 
+    tituloCarousel.textContent = listadoTextosItems[posActual].titulo
+    parrafoCarousel.textContent = listadoTextosItems[posActual].parrafo //acedidendo a propiedad ej:parrafo
 
-},3000)
+
+    carouselTexto.style.animationName = 'fadein'
+    carouselTexto.style.animationDuration = '1s'
+
+
+   setTimeout(()=> {
+     carouselTexto.style.animationName = 'fadeout'
+    carouselTexto.style.animationDuration = '.5s'
+   },2800)
+   
+
+}, 3000)
 
 
 
